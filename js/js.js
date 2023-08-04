991f1edd41f5e34dcb1dc157adc8dd8eb82f5918
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-08-04 18:17:14 +0300

    Final Edit

---
+++ js/js.js
@@ -0,0 +1,21 @@
+var h={
+    "name" : "Abdelslam",
+    "email" : "Abdelslam.ahmed.elkholy@gmail.com",
+    "address" : "22-st-monoufia",
+    "password" : "123456asd",
+    "photo" : "jjj.jpeg",
+    "isAdmin" : true,
+    "isHost" : true,
+    "reservations" : [
+        "ref"
+    ],
+    "wishlist" : [
+        "sssss",
+        "ddddd",
+        "daaaaaa"
+    ],
+    "isConfirmed" : {
+        "email" : true,
+        "identy" : true
+    }
+}
